5f1fd23ffb31ba76fa5558018e8c82714dfe44c1
huylethanh <lethanhhuy6285@gmail.com>
2023-05-07 22:29:34 +0700

    load verify data

--- lib/screens/cycle_counts/screen_daily/daily_verify_cycle_count_screen.dart
+++ lib/screens/cycle_counts/screen_daily/daily_verify_cycle_count_screen.dart
@@ -1,15 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:stacked/stacked.dart';
+import 'package:warehouse_app/screens/cycle_counts/screen_daily/daily_verify_cycle_count_screen_view_model.dart';
+import 'package:warehouse_app/utils/utils.dart';
+import 'package:warehouse_app/widgets/widgets.dart';
 
 class DailyVerifyCycleCountScreen extends StatelessWidget {
   const DailyVerifyCycleCountScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Xác nhận kiểm kê thường nhật"),
-          centerTitle: true,
-        ),
-        body: const Placeholder());
+    return ViewModelBuilder.reactive(
+      viewModelBuilder: () {
+        return DailyVerifyCycleCountScreenViewModel();
+      },
+      onViewModelReady: (DailyVerifyCycleCountScreenViewModel viewModel) {
+        viewModel.loadData();
+      },
+      builder: (BuildContext context,
+          DailyVerifyCycleCountScreenViewModel viewModel, _) {
+        if (viewModel.isBusy) {
+          return const LoadingWidget();
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Xác nhận kiểm kê thường nhật"),
+            centerTitle: true,
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    viewModel.reloadData();
+                  },
+                  child: const Text("Refresh"))
+            ],
+          ),
+          body: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    BarcodeScanner(
+                      value: viewModel.scannedBarcode,
+                      finishScanned: (code) {
+                        viewModel.processInput(context, code);
+                      },
+                      labelText: viewModel.getScanMessage(),
+                      onBarcodeValueChanges: (value) {
+                        viewModel.scannedBarcode = value;
+                      },
+                      cargoSelectedChanges: viewModel.cargoSelectedChanges,
+                      cargoSelected: viewModel.gettingCargo,
+                    ),
+                    Flexible(
+                      child: _body(context, viewModel),
+                    ),
+                  ],
+                ),
+              ),
+              if (viewModel.isProcessing) const BlurLoadingWidget()
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _body(
+      BuildContext context, DailyVerifyCycleCountScreenViewModel viewModel) {
+    if (viewModel.started == null) {
+      return _locations(context, viewModel);
+    }
+
+    return SizedBox();
+  }
+
+  Widget _locations(
+      BuildContext context, DailyVerifyCycleCountScreenViewModel viewModel) {
+    final locations = viewModel.partnerDetail?.cycleCountLocations ?? [];
+
+    if (locations.isEmpty) {
+      return const EmptyWidget();
+    }
+
+    return ListView.builder(
+      itemCount: locations.length,
+      itemBuilder: (BuildContext context, int index) {
+        final item = locations[index];
+
+        return RoundedContainer(
+            key: Key(item.cycleCountId!.toString()),
+            margin: const EdgeInsets.only(bottom: 8),
+            backgroundColor: AppColor.color3D3D3D,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                listData("Vị trí", item.locationCode ?? "", Colors.green),
+                listData("SL Tồng", item.totalQty, Colors.blue),
+                listData("SL Đếm", item.totalCountingQty, Colors.orange),
+                listData("SL Lệch", item.totalDifferentQty, Colors.red),
+              ],
+            ));
+      },
+    );
+  }
+
+  Widget listData(String label, dynamic data, Color dataTextColor) {
+    return Column(children: [
+      Text(label),
+      const SizedBox(
+        height: 8,
+      ),
+      Text(
+        "$data",
+        style: TextStyle(color: dataTextColor, fontSize: 18),
+      ),
+    ]);
   }
 }
--- lib/screens/cycle_counts/screen_daily/daily_verify_cycle_count_screen_view_model.dart
+++ lib/screens/cycle_counts/screen_daily/daily_verify_cycle_count_screen_view_model.dart
@@ -1,5 +1,43 @@
-import 'package:warehouse_app/base/view_models/index.dart';
+import 'package:warehouse_app/models/cycle_count_constain.dart';
+import 'package:warehouse_app/models/models.dart';
 
-class DailyVerifyCycleCountScreenViewModel extends ViewModelBase {
-  //
+import '../models/cycle_count_view_model_base.dart';
+
+class DailyVerifyCycleCountScreenViewModel extends CycleCountViewModelBase {
+  PartnerCycleCount? partnerDetail;
+
+  void loadData() async {
+    setBusy(true);
+    await getLocations();
+    setBusy(false);
+  }
+
+  void reloadData() async {
+    setProcessing(true);
+    await getLocations();
+    setProcessing(false);
+  }
+
+  String getScanMessage() {
+    if (started != null) {
+      return "Quét mã sản phẩm/Serial/Barcode";
+    }
+
+    if (partnerDetail != null) {}
+
+    return "Quét mã vị trí";
+  }
+
+  Future<void> getLocations() async {
+    final partner = await partnerUseCase.getPartners(DAILY, VERIFY_CYCLE_COUNT);
+    if (partner == null || partner.isEmpty) {
+      //   _partnerDetail.postValue(Resource.Success(partnerView.initEmptyPartner()))
+      return;
+    }
+
+    partnerDetail = await partnerUseCase.getPartnerDetail(
+        partner.first.cycleCountId!,
+        roundNumber: VERIFY_CYCLE_COUNT);
+    partnerView = partnerDetail!.toView();
+  }
 }
